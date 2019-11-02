b3bcd269ae9a11e9e083845f0f7a930968cc9da3
Joe Warren <joe.warren7@protonmail.com>
2019-11-02 08:46:02 -0500

    fix: add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,11 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-43084370-3",
+      },
+    },
   ],
 }
